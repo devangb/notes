fc1a6864d2f46dcca7cddd0d240f66213bd3a99e
Devang Bacharwar <devang.bacharwar@gmail.com>
2015-12-14 02:28:45 +0530

    Function skeletons and readme

--- notes.py
+++ notes.py
@@ -4,8 +4,42 @@ import click
 def cli():
 	pass
 
+
+@cli.command()
+def init():
+	pass
+
+
+
+@cli.command()
+@click.argument('branch',required=True)
+def checkout(branch):
+	pass
+
+
 @cli.command()
 @click.argument('title',required=True)
 @click.argument('content',required=True)
 def new(title, content):
 	click.echo(title + ' ' + content)
+
+
+@cli.command()
+def merge():
+	pass
+
+@cli.command()
+def show():
+	pass
+
+@cli.command()
+def edit():
+	pass
+
+@cli.command()
+def archive():
+	pass
+
+@cli.command()
+def delete():
+	pass
